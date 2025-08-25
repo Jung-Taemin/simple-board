b80361120bd12bc2287f8ecf60146ede6592b289
taemin <jungtaemin1214@naver.com>
2025-08-25 17:01:04 +0900

    changes

---
+++ src/test/java/me/jungtaemin/simpleboard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jungtaemin.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
