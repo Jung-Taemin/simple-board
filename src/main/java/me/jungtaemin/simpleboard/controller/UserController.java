31620941863cd8d530640eab1bd83b436f561672
taemin <jungtaemin1214@naver.com>
2025-08-25 17:55:14 +0900

    changes

--- src/main/java/me/jungtaemin/simpleboard/controller/UserController.java
+++ src/main/java/me/jungtaemin/simpleboard/controller/UserController.java
@@ -1,9 +1,12 @@
 package me.jungtaemin.simpleboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.jungtaemin.simpleboard.dto.LoginRequestDto;
+import me.jungtaemin.simpleboard.dto.LoginResponseDto;
 import me.jungtaemin.simpleboard.dto.SignupRequestDto;
 import me.jungtaemin.simpleboard.dto.SignupResponseDto;
 import me.jungtaemin.simpleboard.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,11 @@ public class UserController {
     @PostMapping("/signup")
     public SignupResponseDto signup(@RequestBody SignupRequestDto requestDto) {
         return userService.signup(requestDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto) {
+        LoginResponseDto response = userService.login(requestDto);
+        return ResponseEntity.ok(response);
     }
 }
